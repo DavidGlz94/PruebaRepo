a806d2369463735170518e32592c916267a706cf
DavidGlz94 <davidglz94@outlook.com>
2019-07-25 13:23:14 -0500

    lo importe al repositorio

---
+++ MavenPractica1/src/main/java/com/mavenpractica1/app/MavenHolaMundo.java
@@ -0,0 +1,11 @@
+package com.mavenpractica1.app;
+
+public class MavenHolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo maven");
+		
+	}
+
+}
